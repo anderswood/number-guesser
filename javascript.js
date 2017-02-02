cb78c98af3a8e7006b4e64fce5ce6819a674d190
Anders Wood <anderswood@gmail.com>
2017-02-01 22:44:25 -0700

    Updated all files and added readme

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
+
 var guessBtn = document.querySelector('#guess');
 var clearBtn = document.querySelector('#clear');
 var resetBtn = document.querySelector('#reset');
-
+var currentGuess;
 var secretNumber;
 
 //when the page loads a random number is generated and assigned to secretNumber
@@ -11,32 +12,82 @@ window.onload = function(){
 
 //submit guess and display the guess
 guessBtn.addEventListener('click', function(){
-  var currentGuess = document.getElementById('input');
-  var guessDisplay = document.querySelector('p');
-  currentGuess = parseInt(currentGuess.value,10);
-  guessDisplay.innerText = currentGuess;
-  var feedback = document.querySelector('#feedback');
-  console.log(secretNumber);
-  if (currentGuess > secretNumber) {
-    feedback.innerText = "That is too high";
-  } else if (currentGuess < secretNumber){
-    feedback.innerText = "That is too low";
-  } else if (currentGuess == secretNumber) {
-    feedback.innerText = "BOOM!";
-  } else {
-    console.log("somethings is wrong here");
-  }
+  var currentGuess = document.getElementById('input').value;
+  currentGuess = parseInt(currentGuess,10);
+  resetBtn.disabled = false;
+  screenGuess(currentGuess,secretNumber);
 })
 
+//Review the user's guess (1 and 2) and provide
+//appropropriate feedback (3)
+function screenGuess(guess, secret){
+  // 1. Check if input is a number
+  if (Number.isNaN(guess)) {
+    alert("Please enter a number");
+    return;
+  }
+
+  // 2. Check if the range is valid and the input is within it
+  var minValue = document.querySelector('#min-value').value;
+  var maxValue = document.querySelector('#max-value').value;
+  if (minValue - maxValue > 0) {
+    alert("Please input valid min and max values");
+    return;
+  } else if (guess < minValue || guess > maxValue) {
+    alert("Please provide a guess within the min and max values");
+    return;
+  }
+
+  // 3. Provide appropriate feedback
+  var guessDisplay = document.querySelector('p');
+  guessDisplay.innerText = guess;
+  var feedback = document.querySelector('#feedback');
+  document.querySelector('#min-value').setAttribute("disabled", "true");
+  document.querySelector('#max-value').setAttribute("disabled", "true");
+  console.log("secret number: "+ secret);
+  if (guess > secret) {
+    feedback.innerText = "That is too high";
+  } else if (guess < secret){
+    feedback.innerText = "That is too low";
+  } else if (guess == secret) {
+    feedback.innerText = "BOOM!";
+    minValue = minValue - 10;
+    document.querySelector('#min-value').value = minValue;
+    console.log(maxValue);
+    maxValue = maxValue*1 + 10;
+    document.querySelector('#max-value').value = maxValue;
+    secretNumber = randNum();
+  }
+}
+
 function randNum(){
-  var number = Math.floor(Math.random()*100)+1;
-  console.log(number+" randomly gen number");
+  var minValue = document.querySelector('#min-value').value;
+  var maxValue = document.querySelector('#max-value').value;
+  minValue = parseInt(minValue,10);
+  maxValue = parseInt(maxValue,10);
+  var number = Math.floor(Math.random()*(maxValue - minValue)) + minValue;
   return number;
 }
-// function validate(){
-//
-// }
 
-// clearBtn.addEventListener('click', function(){
-//   currentGuess.value = "";
-// })
+clearBtn.addEventListener('click', function(){
+  document.querySelector('p').innerText = "?";
+  document.querySelector('#feedback').innerText = " ";
+  document.querySelector('#input').value = "";
+  guessBtn.disabled = true;
+  clearBtn.disabled = true;
+})
+
+//Reset the game by reloading the page
+resetBtn.addEventListener('click', function(){
+  window.location.reload();
+})
+
+//Enable the buttons once fields are populated
+document.getElementById('input').addEventListener('keyup',function(){
+  guessBtn.disabled = false;
+  clearBtn.disabled = false;
+  if (document.getElementById('input').value == "") {
+  guessBtn.disabled = true;
+  clearBtn.disabled = true;
+  }
+})
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Mod 1 Project 1: Number Guesser
+
+The number guesser is a game in which the user guesses a number between the number ranges on the screen. The game provides feedback - higher or lower- until the user accurately guesses the number. When the user has honed in on the correct number, the game says "BOOM!", resets the secret number and expands the range of numbers.
+
+The user can input their guess and adjust the min and max values at the beginning of the game. The user can also press the following buttons when they are enabled:
+#####"Guess" button: submits the user's guess and is enables when a value is present in the in the "Enter your guess" field.
+#####"Clear" button: clears the "Enter your guess" field.
+#####"Reset" button: resets the game, including the random number and min and max range.
